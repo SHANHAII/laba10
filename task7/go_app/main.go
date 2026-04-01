66c0d6896595a0baa3414c5fd3f140d13211a8c5
SHANHAII <132259516+SHANHAII@users.noreply.github.com>
2026-04-01 20:35:00 +0300

    task7/go: implement SIGINT handling and srv.Shutdown

---
+++ task7/go_app/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	srv := &http.Server{Addr: ":8082", Handler: r}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit 
+
+	log.Println("Shutting down server...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+	log.Println("Server exiting")
+}
